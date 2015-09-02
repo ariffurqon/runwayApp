4a9718167fb1bdcdbacc28609da353cb8bb73464
ariffurqon <arif.furqon@gmail.com>
2015-09-01 23:02:05 -0700

    add upvotes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = angular.module('runwayApp', []);
 app.controller('MainCtrl', ['$scope', function($scope){
   $scope.test = 'Hello lover!';
 
+  //display posts
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -11,10 +12,16 @@ app.controller('MainCtrl', ['$scope', function($scope){
     {title: 'post 5', upvotes: 4}
   ];
 
+  //add posts
   $scope.addPost = function(){
   	if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({title: $scope.title, upvotes: 0});
     $scope.title = '';
+  };
+
+  //enable upvoting
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
   };
 
 }]);
