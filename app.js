66a519713040206cea0d8c5f2b0a5a5a3960e13b
ariffurqon <arif.furqon@gmail.com>
2015-09-01 23:15:57 -0700

    add link to go along with posts

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.controller('MainCtrl', ['$scope', function($scope){
   //add posts
   $scope.addPost = function(){
   	if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
 
   //enable upvoting
