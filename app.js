080730620da85533818cec536376789120355202
ariffurqon <arif.furqon@gmail.com>
2015-09-02 23:30:22 -0700

    add comment on top of comment

--- app.js
+++ app.js
@@ -58,6 +58,16 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
 
 app.controller('PostsCtrl', ['$scope','$stateParams','posts', function($scope, $stateParams, posts){
   $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
 }]);
 
 
