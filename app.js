a9abe5b1383a2cdc92c7dca424755ac99e35868b
ariffurqon <arif.furqon@gmail.com>
2015-09-02 23:25:08 -0700

    add templates and comments

--- app.js
+++ app.js
@@ -1,24 +1,49 @@
-var app = angular.module('runwayApp', []);
+var app = angular.module('runwayApp', ['ui.router']);
 
-app.controller('MainCtrl', ['$scope', function($scope){
+app.config(['$stateProvider','$urlRouterProvider', 
+  function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+
+  .state('posts', {
+    url: '/posts/{id}',
+    templateUrl: '/posts.html',
+    controller: 'PostsCtrl'
+  })
+
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function(){
+  var x = {
+    posts: []
+  };
+  return x;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
   $scope.test = 'Hello lover!';
 
   //display posts
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  $scope.posts = posts.posts;
 
   //add posts
   $scope.addPost = function(){
-  	if(!$scope.title || $scope.title === '') { return; }
+    if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+          comments: [
+            {author: 'Joe', body: 'Cool post!', upvotes: 0},
+            {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+          ]
     });
     $scope.title = '';
     $scope.link = '';
@@ -30,3 +55,10 @@ app.controller('MainCtrl', ['$scope', function($scope){
   };
 
 }]);
+
+app.controller('PostsCtrl', ['$scope','$stateParams','posts', function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+}]);
+
+
+
