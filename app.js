29c2ac11c1fe428944562f2666c67a48d7f46479
ariffurqon <arif.furqon@gmail.com>
2015-09-03 10:53:22 -0700

    some css touch up

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
         link: $scope.link,
         upvotes: 0,
           comments: [
-            {author: 'Joe', body: 'Cool post!', upvotes: 0},
+            {author: 'Joe', body: 'Amazing design!', upvotes: 0},
             {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
           ]
     });
