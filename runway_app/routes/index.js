bcc295ce1a988f0707fc9882f6c57e3745617f95
ariffurqon <arif.furqon@gmail.com>
2015-09-03 22:42:36 -0700

    comments basic routes completed

--- runway_app/routes/index.js
+++ runway_app/routes/index.js
@@ -28,22 +28,39 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
-// Preload comment objects on routes with ':comment'
+// Preload post objects on routes with ':post'
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
 
   query.exec(function (err, post){
     if (err) { return next(err); }
-    if (!post) { return next(new Error('can\'t find post')); }
+    if (!post) { return next(new Error("can't find post")); }
 
     req.post = post;
     return next();
   });
 });
 
+// Preload comment objects on routes with ':comment'
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 // return a single post
-router.get('/posts/:post', function(req, res) {
-  res.json(req.post);
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
 });
 
 // upvote a post
@@ -55,4 +72,32 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
+// create a new comment
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+// upvote a comment
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+
 module.exports = router;
+
